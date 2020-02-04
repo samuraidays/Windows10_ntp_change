90ab5ca2b2d1c48113dc338a1d0d35bbcea35669
Takashi Hasegawa <1samuraiboy@gmail.com>
2020-02-05 05:59:32 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ntpサーバ同期先を変更するPowerShellスクリプト
+Windows10デバイスのntpサーバ同期先を、
+ntp.nict.jpに変更します。
---
+++ ntp_change.ps1
@@ -0,0 +1,38 @@
+﻿function RegSet( $RegPath, $RegKey, $RegKeyType, $RegKeyValue ){
+    # レジストリありなし確認
+    $Elements = $RegPath -split "\\"
+    $RegPath = ""
+    $FirstLoop = $True
+    foreach ($Element in $Elements ){
+        if($FirstLoop){
+            $FirstLoop = $False
+        }
+        else{
+            $RegPath += "\"
+        }
+        $RegPath += $Element
+        if( -not (test-path $RegPath) ){
+            echo "Add Registry : $RegPath"
+            md $RegPath
+        }
+    }
+
+    # Keyありなし確認
+    $Result = Get-ItemProperty $RegPath -name $RegKey -ErrorAction SilentlyContinue
+    # キーがあった時
+    if( $Result -ne $null ){
+        Set-ItemProperty $RegPath -name $RegKey -Value $RegKeyValue
+    }
+    # キーがなかった時
+    else{
+        # キーを追加する
+        New-ItemProperty $RegPath -name $RegKey -PropertyType $RegKeyType -Value $RegKeyValue
+    }
+    Get-ItemProperty $RegPath -name $RegKey
+}
+
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers"
+$RegKey = "1"
+$RegKeyType = "String"
+$RegKeyValue = "ntp.nict.jp"
+RegSet $RegPath $RegKey $RegKeyType $RegKeyValue
